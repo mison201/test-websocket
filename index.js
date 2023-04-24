2faf20860aca61cb16b8d68199c4d067175f07d4
mison201 <mison201@gmail.com>
2023-04-24 09:35:15 +0700

    chore: add get block data

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-import { ethers, formatEther } from "ethers";
+import { ethers, formatEther, id } from "ethers"
 
 const provider = new ethers.WebSocketProvider(
-  "wss://eth-goerli.g.alchemy.com/v2/cpmRyCYZJDkdte237RoYGX17HYpkG-th"
-);
+  "wss://eth-goerli.g.alchemy.com/v2/cpmRyCYZJDkdte237RoYGX17HYpkG-th",
+)
 
-provider.on("block", (blockNumber) => {
-  console.log("New Block: " + blockNumber);
-});
+provider.on("block", async (blockNumber) => {
+  const block = await provider.getBlock(blockNumber)
+  console.log("New block:", block)
+  console.log("Transactions:", block.transactions)
+})
 const balance = await provider.getBalance(
-  "0x573657f2727594cB872cda53238287b7746Df675"
-);
-console.log("balance :>> ", formatEther(balance));
+  "0x0000000000000000000000000000000000000000",
+)
+console.log("balance :>> ", formatEther(balance))
+
+let filter = {
+  address: "0x0000000000000000000000000000000000000000",
+  topics: [id("Transfer(address,address,uint256)")],
+}
+provider.on(filter, (log, event) => {
+  // Emitted whenever a DAI token transfer occurs
+  console.log("log :>> ", log)
+})
